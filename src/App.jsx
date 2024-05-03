392768a1aa15c5c2a32a97eed2b2cb800bfc3354
Kyle Nurse <kyle.nurse@ryerson.ca>
2024-05-03 14:27:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <header id="header">
+      <img src="investment-calculator-logo.png"></img>
+      <h1 >React Investment Calculator</h1>
+    </header>
+  )
+
+}
+
+export default App
